c9b19502948d8e60a2a7009ae5a4cff184ecf460
Akshay <akshaykuruvinashetti478@gmail.com>
2024-01-26 20:01:17 +0530

    Code for Biggest of three Numbers

--- largest.java
+++ largest.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
-
-public class largest {
+class largest
+{
     public static void main(String args[])
     {
-        Scanner in =new Scanner(System.in);
-        System.out.println("Enter Num1:");
-        float a=in.nextFloat();
-        System.out.println("Enter Num2:");
-        float b=in.nextFloat();
-        System.out.println("Enter Num3:");
-        float c=in.nextFloat();
-        if(a>b && a>c)
-        {
-            System.out.println("A is Largest Number");
+       Scanner in =new Scanner(System.in);
+       System.out.println("Enter Num1:")
+       float a=in.nextFloat();
+       System.out.println("Enter Num2:")
+       float b=in.nextFloat();
+       System.out.println("Enter Num3:")
+       float c=in.nextFloat();
+       void large(){
+          if(a>b && a>c)
+          {
+            System.out.println("A is Biggest")
+ 
+          }
+          else if(b>a && b>c)
+          {
+            System.out.println("B is Biggest")
+ 
+          }
+          else
+          {
+            System.out.println("c is Biggest")
+          }
 
         }
-        else if(b>a && b>c)
-        {
-            System.out.println("B is Largest Number");
-
-        }
-        else
-        {
-            System.out.println("B is Largest Number");
-        }
-
-
     }
 
 }
